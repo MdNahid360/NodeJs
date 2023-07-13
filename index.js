6db319c5e891ba8bf351b0500b82b7d3c258f8bd
md nahid <mdnahid360s@gmail.com>
2023-07-13 16:54:42 +0600

    path method

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const vendor = require('./test.js')
-const _= require('lodash');
-console.table(vendor.a);
-console.table('data is :::::::: ' + _.last(vendor.data));
-vendor.user();
+
+const path = require('path');
+const filename = "F:/WEB DEVELOPMENT/React(2022)/Mern Practice/Practice/Simple_node/test.js";
+
+
+const file = path.basename(filename);
+const dir = path.dirname(filename);
+const ext = path.extname(filename);
+// const frmt = path.format(filename);
+// const absolute = path.isAbsolut(filename);
+// const join = path.join(filename);
+// const normalize = path.normalize(filename);
+// const parse = path.parse(filename);
+// const posix = path.posix(filename);
+// const relative = path.relative(filename);
+// const resolve = path.relative(filename);
+// const sep = path.sep(filename);
+// const win = path.win32(filename);
+
+
+console.log(`path information ::: `, file);
