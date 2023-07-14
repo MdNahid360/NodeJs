03c9034c88181c2f40df74eeb2f497fa4eee3243
md nahid <mdnahid360s@gmail.com>
2023-07-14 20:50:53 +0600

    stream with read & write

--- index.js
+++ index.js
@@ -1,38 +1,21 @@
 const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res)=>{
-    if (req.url === '/') {
-        res.write('<html><head><title>Form</title></head></html>');
-        res.write(' <body><form method="post" action="/message"><input name="message" /></form></body>')
-        res.end();
-    }else if(req.url === '/message' && req.method === 'POST'){
-        const body = []
-        req.on('data', (chunk)=>{
-           body.push(chunk);
-        })
-        req.on('end',()=>{
-            console.log('finished!!!');
-            const parseBody = Buffer.concat(body).toString();
-            console.log(parseBody);
-            res.write('Thanks for sending!!!');
-            res.end();
-        });
-    }else if(req.url === '/users'){
-        res.write('all user information is here');
-        res.end();
-    }else{
-        res.write('NOT FOUND');
-        res.end();
-    }
-});
+const server =http.createServer((req, res) => {
+    const readStream = fs.createReadStream(__dirname+'/bigData.txt', 'utf8');
+
+    readStream.pipe(res)
+})
 
 server.listen(3000)
 
-console.log('listening on port 3000');
 
 // const fs = require('fs');
 // const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf8');
+// const ourWriteStream = fs.createWriteStream(`${__dirname}/outputData.txt`, 'utf8');
 
-// ourReadStream.on('data', (data) =>{
-//    console.log(data);
-// })
+// // ourReadStream.on('data', (chunk) =>{
+// //    ourWriteStream.write(chunk);
+// // });
+
+// ourReadStream.pipe(ourWriteStream)
