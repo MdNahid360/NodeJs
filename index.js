903f2245d36ecfef1bd43ec2817e48c24ac1d643
md nahid <mdnahid360s@gmail.com>
2023-07-14 15:18:33 +0600

    node http

--- index.js
+++
@@ -1,9 +0,0 @@
-const School = require('./event')
-//register a listener for bellRing
-
-const school = new School();
-
-school.on('bellRing', function(period){
-    console.log(`shahin event k dore fel!!!, because ${period}`);
-});
-school.startPeriod()
